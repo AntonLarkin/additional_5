0f4178e36d84a517698a60f45d7d4f87cb854230
Anton <anton11061998@gmail.com>
2018-02-27 12:03:22 +0300

    create project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-}
+    str=str.split("");
+    var del = false;
+    var isop= true;
+    while(!del&&isop){
+        isop = false;
+        for(var i=0;i<str.length;i++){
+            if(str[i]=='('&& !del) {str.splice(i,1); del=true; isop=true;}
+            if(str[i]==')'&& del) {str.splice(i,1); del=false;}
+
+    }
+
+     }var isop= true;
+     while(!del&&isop){
+        isop = false;
+        for(var i=0;i<str.length;i++){
+            if(str[i]=='['&& !del) {str.splice(i,1); del=true; isop=true;}
+            if(str[i]==']'&& del) {str.splice(i,1); del=false;}
+
+    }
+
+     }var isop= true;
+     while(!del&&isop){
+        isop = false;
+        for(var i=0;i<str.length;i++){
+            if(str[i]=='{'&& !del) {str.splice(i,1); del=true; isop=true;}
+            if(str[i]=='}'&& del) {str.splice(i,1); del=false;}
+
+    }
+
+     }var isop= true;
+     while(!del&&isop){
+        isop = false;
+        for(var i=0;i<str.length;i++){
+            if(str[i]=='|'&& !del) {str.splice(i,1); del=true; isop=true;}
+            if(str[i]=='|'&& del) {str.splice(i,1); del=false;}
+
+    }
+
+     }
+     if(str.length>0) return false;
+     else return true;
+   }
